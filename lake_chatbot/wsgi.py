f377886b7e92314fec414c860c1beb2abfd9376d
Darshan-vitg <darshan.vaghasiya@vitg.us>
2025-04-09 17:32:32 -0400

    Update wsgi.py

--- lake_chatbot/wsgi.py
+++ lake_chatbot/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lake_chatbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hmdaV2.settings')
 
 application = get_wsgi_application()
