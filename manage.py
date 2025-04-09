0c9f174c2197e3c84c55ae2e1e871f3bb5632883
Darshan-vitg <darshan.vaghasiya@vitg.us>
2025-04-09 17:30:35 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lake_chatbot.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hmdaV2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
